6b629d75180b1cd3ca406834b6e824982536e220
Marco <marcobaruco2091@gmail.com>
2025-02-16 15:31:02 -0300

    operadores

--- ExemploSintaxeBasica/Program.cs
+++ ExemploSintaxeBasica/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+/*int variavelDoTipoInteiro = 8;
+float variavelDoTipoFloat = 8.9f;
+bool variavelDoTipoBool = false;
+decimal variavelDoTipoDecimal = 9.8m;
+*/
+const decimal pi = 3.14m;
+
+//pi = 9;
+
+Console.WriteLine(pi);
---
+++ operadores/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 2;
+int y = 5;
+
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
+Console.WriteLine(x % y);
+
+float valor = 20;
+
+Console.WriteLine(valor += 1);
+Console.WriteLine(valor -= 1);
+Console.WriteLine(valor *= 1);
+Console.WriteLine(valor /= 1);
+Console.WriteLine(valor %= 1);
+
+float z = 5;
+
+Console.WriteLine(z += 1);
+Console.WriteLine(z -= 1);
+Console.WriteLine(z *= 1);
+Console.WriteLine(z /= 1);
+Console.WriteLine(z %= 1);
