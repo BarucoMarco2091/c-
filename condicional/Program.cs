3f198e4fa49a3ebc98de7d71a2bb4ac4607d0282
Marco <marcobaruco2091@gmail.com>
2025-02-17 08:58:26 -0300

    condicional

---
+++ condicional/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int idadeDaAna = 45;
+int idadeDoMarco = 33;
+
+if(idadeDaAna >= idadeDoMarco) 
+{
+    Console.WriteLine("A Aninha é mais velha que o Marco");
+}
+else {
+    Console.WriteLine("O Marco é mais novo que a Aninha");
+}
+
+// Ternário
+
+int x = 6;
+int y = 9;
+
+Console.WriteLine((x > y)? "sim": "nao");
